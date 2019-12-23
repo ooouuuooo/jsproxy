524f25450fa6d14f8905d2932b381b052bc6bd89
ooouuuooo <40920985+ooouuuooo@users.noreply.github.com>
2019-12-24 04:07:59 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -53,8 +53,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
-  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  // node_default: 'mysite',
+  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
