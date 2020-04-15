6506aa88043856c0514f0fa3993d0c16ea430908
ooouuuooo <40920985+ooouuuooo@users.noreply.github.com>
2020-04-15 13:20:34 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -16,7 +16,6 @@ jsproxy_config({
       label: '当前站点',
       lines: {
         [location.host]: 1,
-        //'b.oouuoo.xyz': 1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -32,7 +31,7 @@ jsproxy_config({
         //'b.007.workers.dev': 1,
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
-        'b.oouuoo.xyz': 1,
+        [location.host]: 1,
       }
     }
   },
